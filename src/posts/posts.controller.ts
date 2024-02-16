a72f07a2eecb96373957a1f6ae39db7c2027dbf9
Yazan Farrah <yazanfarrah03@gmail.com>
2024-02-16 23:31:37 +0300

    Initial commit

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    console.log(createPostDto);
+    return this.postsService.create(createPostDto);
+  }
+
+  @Get()
+  findAll(@Query() queryObject: any) {
+    console.log(queryObject);
+    return this.postsService.findAll();
+  }
+
+  @Get(':id/:age')
+  @HttpCode(HttpStatus.ACCEPTED)
+  findOne(@Param('id') id: string, @Param('age') age: String) {
+    console.log(`id is: ${id}, age is: ${age}`);
+    return this.postsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(+id);
+  }
+}
